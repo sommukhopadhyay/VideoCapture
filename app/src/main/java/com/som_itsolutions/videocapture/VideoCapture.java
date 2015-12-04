ee89af320b2231fd2e6a05fd3f3e5d387619a06c
som <som.mukhopadhyay@gmail.com>
2015-12-04 07:39:36 +0530

    Initial commit

---
+++ app/src/main/java/com/som_itsolutions/videocapture/VideoCapture.java
@@ -0,0 +1,147 @@
+package com.som_itsolutions.videocapture;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.io.File;
+import java.io.IOException;
+
+public class VideoCapture extends Activity implements View.OnClickListener, SurfaceHolder.Callback{
+    MediaRecorder recorder;
+    SurfaceHolder holder;
+    boolean recording = false;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        recorder = new MediaRecorder();
+        initRecorder();
+        setContentView(R.layout.activity_video_capture);
+
+        SurfaceView cameraView = (SurfaceView) findViewById(R.id.surfaceView);
+        holder = cameraView.getHolder();
+        holder.addCallback(this);
+        //holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        cameraView.setClickable(true);
+        cameraView.setOnClickListener(this);
+    }
+
+    private void initRecorder() {
+        recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+        String[] ids = {""};
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            ids = manager.getCameraIdList();
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        CamcorderProfile cpLow = CamcorderProfile
+                .get(Integer.parseInt(ids[0]),CamcorderProfile.QUALITY_LOW);
+        recorder.setProfile(cpLow);
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory()+ File.separator+
+                Environment.DIRECTORY_PICTURES + File.separator + "VideoCapture");
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return;
+            }
+        }
+
+        Log.i("OutputFilePath", mediaStorageDir.getPath());
+        recorder.setOutputFile(mediaStorageDir.getPath() + File.separator + "videocapture.mp4");
+
+        recorder.setMaxDuration(50000); // 50 seconds
+        recorder.setMaxFileSize(5000000); // Approximately 5 megabytes
+    }
+
+    private void prepareRecorder() {
+        recorder.setPreviewDisplay(holder.getSurface());
+
+        try {
+            recorder.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            finish();
+        } catch (IOException e) {
+            e.printStackTrace();
+            finish();
+        }
+    }
+
+    public void onClick(View v) {
+        if (recording) {
+            recorder.stop();
+            recording = false;
+
+            // Let's initRecorder so we can record again
+            initRecorder();
+            prepareRecorder();
+        } else {
+            recording = true;
+            recorder.start();
+        }
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        prepareRecorder();
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (recording) {
+            recorder.stop();
+            recording = false;
+        }
+        recorder.release();
+        finish();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_video_capture, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
